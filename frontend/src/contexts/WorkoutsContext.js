76f5b3cbee7dbd70b3f101581f6959f67888365d
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-04-16 07:26:39 +0100

    Add delete functionality to the frontend

--- frontend/src/contexts/WorkoutsContext.js
+++ frontend/src/contexts/WorkoutsContext.js
@@ -22,6 +22,10 @@ function WorkoutsReducer(state, action) {
       return {
         workouts: [action.payload, ...state.workouts],
       };
+    case 'DELETE_WORKOUT':
+      return {
+        workouts: state.workouts.filter((w) => w._id !== action.payload._id),
+      };
 
     default: {
       throw Error('Unknown action: ' + action.type);
